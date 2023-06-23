a3878c4e99977f4db874cfdb542b2dae8fd624fa
Jorge Luis <Desarrollador@tutanota.com>
2023-06-23 01:30:00 -0500

    actualizar

---
+++ api/main.py
@@ -0,0 +1,262 @@
+from fastapi import FastAPI
+import pandas as pd
+import zipfile
+
+# uvicorn main:app --reload
+# ----------------------------------------------------
+app = FastAPI()
+# ----------------------------------------------------
+
+# Cargar los datasets
+# ----------------------------------------------------
+def extract_data_from_zip():
+    """
+    Extrae y carga los conjuntos de datos desde un archivo comprimido.
+    
+    Returns:
+        datasets_df (DataFrame): Dataframe que contiene los datos finales.
+        crew_df (DataFrame): Dataframe que contiene los datos de equipo de producción.
+        cast_df (DataFrame): Dataframe que contiene los datos de reparto de películas.
+        movie_genres_df (DataFrame): Dataframe que contiene los géneros de películas.
+    """
+    zip_file = '../data/data.zip'
+    
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall('../data/')  # Descomprime los archivos en el directorio '../data/'
+    
+    datasets_df = pd.read_csv('../data/datasets_final.csv')
+    crew_df = pd.read_csv('../data/crew_data.csv')
+    cast_df = pd.read_csv('../data/cast_data.csv')
+    movie_genres_df = pd.read_csv('../data/movie_genres.csv')
+    
+    return datasets_df, crew_df, cast_df, movie_genres_df
+# ----------------------------------------------------
+
+# Rutas para los endpoints
+# ----------------------------------------------------
+@app.get("/cantidad-filmaciones-mes/{mes}")
+async def cantidad_filmaciones_mes_endpoint(mes: str):
+    """
+    Endpoint para obtener la cantidad de filmaciones de películas realizadas en un mes específico.
+    
+    Args:
+        mes (str): Nombre del mes en formato de texto.
+    
+    Returns:
+        dict: Diccionario con el mensaje que indica la cantidad de filmaciones en el mes consultado.
+    """
+    # Convertir el nombre del mes a minúsculas para realizar la comparación
+    mes = mes.lower()
+
+    # Extraer los DataFrames del archivo ZIP
+    datasets_df, _, _, _ = extract_data_from_zip()
+
+    # Filtrar el DataFrame para obtener las filas correspondientes al mes consultado
+    filmaciones_mes = datasets_df[datasets_df['month_time'].str.lower() == mes]
+
+    # Obtener la cantidad de películas estrenadas en el mes consultado
+    cantidad_filmaciones = len(filmaciones_mes)
+
+    mensaje = f"La cantidad de películas estrenadas en {mes.capitalize()} es: {cantidad_filmaciones}"
+    return {"mensaje": mensaje}
+
+# ----------------------------------------------------
+@app.get("/cantidad-filmaciones-dia/{dia}")
+async def cantidad_filmaciones_dia_endpoint(dia: str):
+    """
+    Endpoint para obtener la cantidad de filmaciones de películas realizadas en un día específico.
+    
+    Args:
+        dia (str): Nombre del día en formato de texto.
+    
+    Returns:
+        dict: Diccionario con el mensaje que indica la cantidad de filmaciones en el día consultado.
+    """
+    # Convertir el nombre del día a minúsculas para realizar la comparación
+    dia = dia.lower()
+
+    # Extraer los DataFrames del archivo ZIP
+    datasets_df, _, _, _ = extract_data_from_zip()
+
+    # Filtrar el DataFrame para obtener las filas correspondientes al día consultado
+    filmaciones_dia = datasets_df[datasets_df['day_time'].str.lower() == dia]
+
+    # Obtener la cantidad de películas estrenadas en el día consultado
+    cantidad_filmaciones = len(filmaciones_dia)
+
+    mensaje = f"La cantidad de películas estrenadas en {dia.capitalize()} es: {cantidad_filmaciones}"
+    return {"mensaje": mensaje}
+
+# ----------------------------------------------------
+@app.get("/titulo-de-la-filmacion/{titulo}")
+async def titulo_de_la_filmacion_endpoint(titulo: str):
+    """
+    Endpoint para obtener información de una filmación a partir de su título.
+    
+    Args:
+        titulo (str): Título de la filmación en formato de texto.
+    
+    Returns:
+        dict: Diccionario con el mensaje que contiene el título, año de estreno y score/popularidad de la filmación.
+              En caso de no encontrar la filmación, se devuelve un mensaje de error.
+    """
+    # Convertir el título de la filmación a minúsculas para realizar la comparación
+    titulo = titulo.lower()
+
+    # Extraer los DataFrames del archivo ZIP
+    datasets_df, _, _, _ = extract_data_from_zip()
+
+    # Filtrar el DataFrame para obtener la fila correspondiente al título de la filmación
+    filmacion = datasets_df[datasets_df['title'].str.lower() == titulo]
+
+    if filmacion.empty:
+        return {"mensaje": f"No se encontró la filmación con el título: {titulo}"}
+
+    # Obtener el título, año de estreno y score de la filmación
+    titulo_filmacion = filmacion['title'].iloc[0]
+    año_estreno = filmacion['release_year'].iloc[0]
+    score = round(filmacion['popularity'].iloc[0], 2)
+
+    mensaje = f"La película '{titulo_filmacion}' fue estrenada en el año {año_estreno} con un score/popularidad de {score}"
+    return {"mensaje": mensaje}
+
+# ----------------------------------------------------
+@app.get("/votos-valor-de-la-filmacion/{titulo}")
+async def votos_valor_de_la_filmacion_endpoint(titulo: str):
+    """
+    Endpoint para obtener información sobre los votos y valoraciones de una filmación a partir de su título.
+    
+    Args:
+        titulo (str): Título de la filmación en formato de texto.
+    
+    Returns:
+        dict: Diccionario con el mensaje que contiene el título, año de estreno, cantidad de votos y promedio de votaciones de la filmación.
+              En caso de no encontrar la filmación o no cumplir con la cantidad mínima de votos, se devuelve un mensaje de error.
+    """
+    # Convertir el título de la filmación a minúsculas para realizar la comparación
+    titulo = titulo.lower()
+
+    # Extraer los DataFrames del archivo ZIP
+    datasets_df, _, _, _ = extract_data_from_zip()
+
+    # Filtrar el DataFrame para obtener la fila correspondiente al título de la filmación
+    filmacion = datasets_df[datasets_df['title'].str.lower() == titulo]
+
+    if filmacion.empty:
+        return {"mensaje": f"No se encontró la filmación con el título: {titulo}"}
+
+    # Obtener el título, año de estreno, cantidad de votos y promedio de votaciones de la filmación
+    titulo_filmacion = filmacion['title'].iloc[0]
+    año_estreno = filmacion['release_year'].iloc[0]
+    cantidad_votos = round(filmacion['vote_count'].iloc[0])
+    promedio_votaciones = filmacion['vote_average'].iloc[0]
+
+    if cantidad_votos < 2000:
+        return {"mensaje": f"La filmación '{titulo_filmacion}' no cumple con la condición de tener al menos 2000 votos"}
+
+    mensaje = f"La película '{titulo_filmacion}' fue estrenada en el año {año_estreno}. La misma cuenta con un total de {cantidad_votos} valoraciones, con un promedio de {promedio_votaciones}"
+
+    return {"mensaje": mensaje}
+
+# ----------------------------------------------------
+@app.get("/nombre-actor/{nombre}")
+def nombre_actor(nombre: str):
+    """
+    Endpoint para obtener información sobre un actor a partir de su nombre.
+    
+    Args:
+        nombre (str): Nombre del actor en formato de texto.
+    
+    Returns:
+        dict: Diccionario con el nombre del actor, la cantidad de películas en las que ha participado, el promedio de retorno monetario de las películas
+              y una lista de diccionarios con los títulos y retornos monetarios de las películas en las que ha participado.
+    """
+    datasets_df, _, cast_df, _ = extract_data_from_zip()
+
+    # Filtrar las filas en las que el actor aparece en la columna "cast"
+    actor_movies = cast_df[cast_df['cast'].str.contains(nombre, case=False)]
+    
+    # Obtener los ID de las películas en las que el actor ha participado
+    movie_ids = actor_movies['id'].tolist()
+    
+    # Filtrar el dataset "datasets_df" para obtener los nombres y retornos monetarios de las películas correspondientes
+    movies = datasets_df[datasets_df['id'].isin(movie_ids)]
+    
+    # Obtener la cantidad de películas en las que el actor ha participado
+    movie_count = len(movies)
+    
+    # Obtener el promedio de retorno monetario de las películas
+    average_revenue = round(movies['revenue'].mean(), 2)
+
+    
+    # Crear una lista de diccionarios con los nombres y retornos monetarios de las películas
+    movie_info = []
+    for _, row in movies.iterrows():
+        movie_info.append({
+            "titulo": row['title'],
+            "retorno_monetario": row['revenue']
+        })
+    
+    return {
+        "nombre_actor": nombre,
+        "cantidad_peliculas": movie_count,
+        "promedio_retorno_monetario": average_revenue,
+        "peliculas": movie_info
+    }
+
+# ----------------------------------------------------
+@app.get("/nombre-director/{nombre}")
+def nombre_director(nombre: str):
+    """
+    Endpoint para obtener información sobre un director a partir de su nombre.
+    
+    Args:
+        nombre (str): Nombre del director en formato de texto.
+    
+    Returns:
+        dict: Diccionario con el nombre del director, las ganancias totales de las películas que ha dirigido y una lista de diccionarios
+              con los ID, títulos, años, presupuestos, ingresos y relaciones de las películas dirigidas por el director.
+    """
+    datasets_df, crew_df, _, _ = extract_data_from_zip()
+
+    # Filtrar las filas en las que el director aparece en la columna "crew_name" y "crew_job" contiene "Director"
+    director_movies = crew_df[(crew_df['crew_name'].str.contains(nombre, case=False)) & (crew_df['crew_job'].str.contains("Director"))]
+    
+    # Obtener los ID de las películas en las que el director ha trabajado
+    movie_ids = director_movies['id'].tolist()
+    
+    # Filtrar el dataset "datasets_df" para obtener los nombres, años, presupuestos, ingresos y relación de las películas correspondientes
+    movies = datasets_df[datasets_df['id'].isin(movie_ids)]
+    
+    # Calcular las ganancias sumando todas las relaciones de las películas
+    ganancias = round(movies['return'].sum(), 2)
+    
+    # Crear una lista de diccionarios con los ID, nombres, años, presupuestos, ingresos y relación de las películas
+    movie_info = []
+    for _, row in movies.iterrows():
+        movie_info.append({
+            "id": row['id'],
+            "titulo": row['title'],
+            "anio": row['release_year'],
+            "presupuesto": row['budget'],
+            "ingresos": row['revenue'],
+            "relacion": row['return']
+        })
+    
+    return {
+        "nombre_director": nombre,
+        "ganancias": ganancias,
+        "peliculas": movie_info
+    }
+
+
+
+
+
+
+
+# ----------------------------------------------------
+# Ejecutar la aplicación
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ data/Readme_ETL.md
@@ -0,0 +1,85 @@
+# Proyecto Individual #1
+
+Descripción breve del proyecto.
+
+## Tabla de Contenidos
+
+- [Descripción](#descripción)
+- [Proceso de Limpieza de Datos](#requisitos)
+- [Diccionario de Películas](#diccionario)
+
+## Descripción
+
+El objetivo de este proyecto individual es realizar la limpieza y preparación de datos del conjunto de datos `dataset_movies.csv`, con el propósito de construir una API utilizando FastAPI para realizar consultas de películas. La API permitirá a los usuarios buscar películas por diferentes criterios y obtener información detallada sobre las mismas.
+
+## Proceso de Limpieza de Datos
+
+El proceso de limpieza de datos consistirá en varias etapas:
+
+1. **Carga de Datos** : Se realizará la carga del conjunto de datos `dataset_movies.csv` en un DataFrame utilizando la biblioteca Pandas.
+2. **Eliminación de Columnas Innecesarias** : Se identificarán las columnas que no son relevantes para el objetivo de la API y se eliminarán del DataFrame.
+3. **Limpieza de Datos Faltantes** : Se realizará una exploración del DataFrame para identificar columnas con datos faltantes. Se aplicarán técnicas de imputación o eliminación de filas/columnas dependiendo del caso.
+4. **Extracción de Datos de Columnas Anidadas** : Se analizarán las columnas que contienen datos anidados, como por ejemplo `production_companies` y `production_countries`. Se extraerán los datos relevantes de estas columnas y se crearán columnas separadas en el DataFrame.
+5. **Transformación de Datos** : Se aplicarán transformaciones necesarias a las columnas, como por ejemplo convertir formatos de fechas, eliminar caracteres especiales, normalizar textos, etc.
+6. **Validación y Control de Calidad** : Se realizará una validación exhaustiva de los datos limpios para asegurarse de que cumplan con los requisitos de la API y no contengan errores o inconsistencias.
+
+## Diccionarios
+
+A continuación se muestra un diccionario que describe cada columna en el conjunto de datos del archivo datasets_final.csv:
+
+```python
+column_description = {
+    'id': 'ID de la película',
+    'title': 'Título de la película',
+    'overview': 'Descripción de la película',
+    'popularity': 'Popularidad de la película',
+    'release_date': 'Fecha de lanzamiento de la película',
+    'vote_average': 'Promedio de votos de la película',
+    'vote_count': 'Número de votos de la película',
+    'status': 'Estado de la película',
+    'original_language': 'Idioma original de la película',
+    'runtime': 'Duración de la película en minutos',
+    'budget': 'Presupuesto de la película',
+    'revenue': 'Ingresos generados por la película',
+    'tagline': 'Lema de la película',
+    'id_btc': 'ID de la película en BTC',
+    'name_btc': 'Nombre de la película en BTC',
+    'poster_btc': 'URL del póster de la película en BTC',
+    'backdrop_btc': 'URL del fondo de la película en BTC',
+    'iso_639_1': 'Código ISO 639-1 del idioma',
+    'language_name': 'Nombre del idioma',
+    'release_year': 'Año de lanzamiento de la película',
+    'return': 'Relación entre ingresos y presupuesto de la película',
+    'companies_id': 'ID de las compañías de producción',
+    'companies_name': 'Nombres de las compañías de producción',
+    'countries_iso': 'Códigos ISO de los países de producción',
+    'countries_name': 'Nombres de los países de producción',
+    'mes': 'mes en el que se creo la pelicula',
+    'dia': 'dia en el que se creo la pelicula'
+}
+```
+
+A continuación se muestra un diccionario que describe cada columna en el conjunto de datos del archivo crew_data.csv:
+
+```python
+column_description = {
+    'id': 'ID de la película',
+    'crew_credit_id': 'ID de la credencial del equipo',
+    'crew_department': 'Departamento del equipo',
+    'crew_gender': 'Género del miembro del equipo',
+    'crew_id': 'ID del miembro del equipo',
+    'crew_job': 'Rol del miembro del equipo',
+    'crew_name': 'Nombre del miembro del equipo',
+    'crew_profile_path': 'Ruta del perfil del miembro del equipo'
+}
+
+```
+
+A continuación se muestra un diccionario que describe cada columna en el conjunto de datos del archivo cast_data.csv:
+
+```python
+column_description = {
+    'id': 'ID de la película',
+    'cast': 'Elenco de la película en formato JSON'
+}
+```
